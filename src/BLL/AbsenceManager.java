4aef8cc85e0666de6ff9b0ffda48e3fa4f259b7b
jbn108 <jbuus9@gmail.com>
2021-02-08 08:34:51 +0100

    misc

---
+++ src/BLL/AbsenceManager.java
@@ -0,0 +1,14 @@
+package BLL;
+
+public class AbsenceManager {
+
+    // TODO getStudentAbsence
+
+    // TODO getAllStudentsAbsence
+    // Uses getStudentAbsence
+
+    // TODO getSubjectStatistics
+    // Uses getAllStudentsAbsence
+
+
+}
