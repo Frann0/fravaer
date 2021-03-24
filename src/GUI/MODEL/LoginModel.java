f83e8f9c626e465cadab63229d5d948fd6336500
Frann0 <mikehovedskov@gmail.com>
2021-03-24 13:37:21 +0100

    Added login passthrough
    - Tilføjet nogle javadocs

--- src/GUI/MODEL/LoginModel.java
+++ src/GUI/MODEL/LoginModel.java
@@ -13,5 +13,15 @@ public class LoginModel {
         return myLoginManager.getAllUsers();
     }
  */
+
+    /**
+     * Attempt a login with the given credentials.
+     * @param username The username of the user.
+     * @param password The password of the user.
+     * @return returns a user object if successful, else null.
+     */
+    public User attemptLogin(String username, String password){
+        return myLoginManager.debugAttemptLogin(username,password);
+    }
 }
 
