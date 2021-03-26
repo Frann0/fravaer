4337c8461b1e41dadd31aca851b17176df478c00
jbn108 <jbuus9@gmail.com>
2021-03-26 08:18:20 +0100

    BE work

--- src/BE/Lecture.java
+++ src/BE/Lecture.java
@@ -1,14 +1,24 @@
 package BE;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Lecture {
     private String subjectName;
     private LocalDate lectureDate;
+    private LocalTime lectureTime;
 
-    public Lecture(String subjectName, LocalDate lectureDate){
+    public Lecture(String subjectName, LocalDate lectureDate, LocalTime lectureTime){
         this.subjectName = subjectName;
         this.lectureDate = lectureDate;
+        this.lectureTime = lectureTime;
     }
 
+    public LocalTime getLectureTime() {
+        return lectureTime;
+    }
+
+    public LocalDate getLectureDate() {
+        return lectureDate;
+    }
 }
--- src/DAL/DB/DataGenerator.java
+++ src/DAL/DB/DataGenerator.java
@@ -1,8 +1,15 @@
 package DAL.DB;
 
+import BE.Attendance;
+import BE.Lecture;
+import BE.User;
+
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * TEMPORARY CLASS FOR DATA GENERATION
@@ -10,13 +17,47 @@ import java.time.LocalTime;
 
 public class DataGenerator {
 
+    private final int LECTURE_DURATION = 45;
+    private static List<Lecture> allLectures = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         LectureDAL lectureDAL = new LectureDAL();
 
-        addLectures(lectureDAL);
-        setAttendance(lectureDAL);
+
+
+
+
+
+
+
+        //addLectures(lectureDAL);
+        //setAttendance(lectureDAL);
 
     }
+
+
+
+    public void registerAttendance(User student, LocalDateTime registration){
+
+        LocalDate registrationDate = registration.toLocalDate();
+        LocalTime registrationTime = registration.toLocalTime();
+
+        for(Lecture lecture : allLectures){
+
+            if(lecture.getLectureDate().equals(registrationDate) &&
+            registrationTime.isAfter(lecture.getLectureTime()) &&
+            registrationTime.isBefore(lecture.getLectureTime().plusMinutes(LECTURE_DURATION))){
+
+                student.addAttendance(new Attendance(lecture, true));
+
+            }
+        }
+    }
+
+
+
+
+
 
     private static void setAttendance(LectureDAL lectureDAL) {
         
