4ca508b18472f79dc61d1d0855625bf3bbf3a3a2
DennisPC-bit <denn062g@easv365.dk>
2021-04-09 14:00:19 +0200

    made data generator class
    woo data and stuff

--- src/BLL/AttendanceManager.java
+++ src/BLL/AttendanceManager.java
@@ -11,13 +11,20 @@ public class AttendanceManager {
 
     /**
      * Updates the student's attendance at the given lecture
+     *
      * @param student the student you want to update
      * @param lecture the lecture you want the student to register to
      */
     public static void updateStudentAttendance(Student student, Lecture lecture, boolean isAttended) {
         UserDAL userDAL = new UserDAL();
+        userDAL.addAttendanceRegistration(student.getId(), lecture.getLectureId(), isAttended);
+        for (Attendance attendance : student.getAttendances()) {
+            if (attendance.getLecture() == lecture && attendance.isAttended() != isAttended) {
+                attendance.setAttended(isAttended);
+                return;
+            }
+        }
         student.getAttendances().add(new Attendance(lecture, isAttended));
-        userDAL.addAttendanceRegistration(student.getId(),lecture.getLectureId(), isAttended);
     }
 
 }
